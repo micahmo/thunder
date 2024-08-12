8472ba3fd7b66128e69654a237c599bc91ef8347
Micah Morrison <micahmorrison95@gmail.com>
2024-08-12 15:13:33 -0400

    Highlight last tapped post

--- lib/community/widgets/post_card_view_comfortable.dart
+++ lib/community/widgets/post_card_view_comfortable.dart
@@ -45,6 +45,7 @@ class PostCardViewComfortable extends StatelessWidget {
   final ListingType? listingType;
   final void Function({PostViewMedia? postViewMedia})? navigateToPost;
   final bool indicateRead;
+  final bool isLastTapped;
 
   const PostCardViewComfortable({
     super.key,
@@ -67,6 +68,7 @@ class PostCardViewComfortable extends StatelessWidget {
     required this.markPostReadOnMediaView,
     required this.listingType,
     required this.indicateRead,
+    required this.isLastTapped,
     this.navigateToPost,
   });
 
@@ -102,7 +104,11 @@ class PostCardViewComfortable extends StatelessWidget {
     final bool darkTheme = context.read<ThemeBloc>().state.useDarkTheme;
 
     return Container(
-      color: indicateRead && postViewMedia.postView.read ? theme.colorScheme.onBackground.withOpacity(darkTheme ? 0.05 : 0.075) : null,
+      color: isLastTapped
+          ? theme.colorScheme.primary.withOpacity(0.15)
+          : indicateRead && postViewMedia.postView.read
+              ? theme.colorScheme.onBackground.withOpacity(darkTheme ? 0.05 : 0.075)
+              : null,
       padding: const EdgeInsets.symmetric(vertical: 12.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
